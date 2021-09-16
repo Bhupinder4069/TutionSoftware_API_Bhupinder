e33be1a19e65102928a9f5ef613f7589152d3e32
Bhupinder1992011@gmail.com <Bhupinder1992011@gmail.com>
2021-09-16 23:13:51 +0300

    cchh

---
+++ TutionSoftware_API_Bhupinder/Controllers/SubjectAPIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutionSoftware_API_Bhupinder.Controllers
+{
+    public class SubjectAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TutionSoftware_API_Bhupinder/Data/ApplicationDbContext.cs
+++ TutionSoftware_API_Bhupinder/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TutionSoftware_API_Bhupinder.Models;
 
 namespace TutionSoftware_API_Bhupinder.Data
 {
@@ -12,5 +13,9 @@ namespace TutionSoftware_API_Bhupinder.Data
             : base(options)
         {
         }
+
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
     }
 }
