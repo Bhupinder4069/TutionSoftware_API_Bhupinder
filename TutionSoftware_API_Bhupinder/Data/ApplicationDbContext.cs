620632b3fa613ba8d4a1919d191d2356a9d54735
Bhupinder1992011@gmail.com <Bhupinder1992011@gmail.com>
2021-09-16 16:04:36 +0300

    Add project files.

---
+++ TutionSoftware_API_Bhupinder/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutionSoftware_API_Bhupinder.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
